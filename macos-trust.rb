82ea2549dbfeed22c84a404e50b404a9ab4110a9
Chris Campbell <91504044+texasbe2trill@users.noreply.github.com>
2026-02-07 14:04:50 -0600

    Add macos-trust v0.4.0 formula

---
+++ macos-trust.rb
@@ -0,0 +1,21 @@
+class MacosTrust < Formula
+  include Language::Python::Virtualenv
+
+  desc "Intelligent macOS security scanner with interactive HTML reports"
+  homepage "https://github.com/texasbe2trill/macos-trust"
+  url "https://github.com/texasbe2trill/macos-trust/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "3b82f5cf08cbf6a26d6819b3784d1d9e1ae198ac004ca2e66a199ca4ac33a757"
+  license "MIT"
+  head "https://github.com/texasbe2trill/macos-trust.git", branch: "main"
+
+  depends_on "python@3.11"
+  depends_on macos: :catalina
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_match "0.4.0", shell_output("#{bin}/macos-trust --version")
+  end
+end
