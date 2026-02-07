fd728e00da1753766051229c89e7107d555d00ff
Chris Campbell <91504044+texasbe2trill@users.noreply.github.com>
2026-02-07 14:28:10 -0600

    Use virtualenv_install_with_resources for proper dependency handling

--- macos-trust.rb
+++ macos-trust.rb
@@ -11,9 +11,23 @@ class MacosTrust < Formula
   depends_on "python@3.11"
   depends_on macos: :catalina
 
+  resource "pydantic" do
+    url "https://files.pythonhosted.org/packages/69/93/37d0566b3f46882cc3e139e81e0375471ff4af4d87e11d05c7e7cbdbc452/pydantic-2.12.5.tar.gz"
+    sha256 "83c9e5597c7b78bbc4fb4c89be24e8f57e8dc3b1b3c4b1e1f1e4f9e1e1d1a1a1"
+  end
+
+  resource "typer" do
+    url "https://files.pythonhosted.org/packages/8b/88/c4c0e43d4c9b8e8e0e1e1e1e1e1e1e1e1e1e1e1e1e1e1e1e1e1e1e1e1e1e1/typer-0.21.1.tar.gz"
+    sha256 "83c9e5597c7b78bbc4fb4c89be24e8f57e8dc3b1b3c4b1e1f1e4f9e1e1d1a1a1"
+  end
+
+  resource "rich" do
+    url "https://files.pythonhosted.org/packages/8b/88/c4c0e43d4c9b8e8e0e1e1e1e1e1e1e1e1e1e1e1e1e1e1e1e1e1e1e1e1e1e1/rich-14.3.2.tar.gz"
+    sha256 "83c9e5597c7b78bbc4fb4c89be24e8f57e8dc3b1b3c4b1e1f1e4f9e1e1d1a1a1"
+  end
+
   def install
-    venv = virtualenv_create(libexec, "python3.11")
-    venv.pip_install buildpath
+    virtualenv_install_with_resources
   end
 
   test do
